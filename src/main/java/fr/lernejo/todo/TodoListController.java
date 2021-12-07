678486377447f85f160372f1f6aed9b52ae55fee
angelique2000 <angelique.proux@gmail.com>
2021-12-07 09:23:05 +0100

    Database persistence

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -6,15 +6,31 @@ import java.util.ArrayList;
 
 @RestController
 public class TodoListController {
-    ArrayList<Todo> todoArrayList = new ArrayList<>();
+    private final TodoRepository todoRepository;
+    //ArrayList<Todo> todoArrayList = new ArrayList<>();
+
+    public TodoListController(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
 
     @PostMapping(value = "/api/todo")
-    public void addTodoToTheList(@RequestParam("message") String message, @RequestParam("author") String author){
+    /*public void addTodoToTheList(@RequestParam("message") String message, @RequestParam("author") String author){
         this.todoArrayList.add(new Todo(message, author));
+    }*/
+    public @ResponseBody TodoEntity addTodoToTheBdd(@RequestBody Todo todo){
+        return this.todoRepository.save(new TodoEntity(null, todo.message(), todo.author()));
     }
 
     @GetMapping(value = "/api/todo")
-    public @ResponseBody ArrayList<Todo> getTodoList(){
+    /*public @ResponseBody ArrayList<Todo> getTodoList(){
         return this.todoArrayList;
+    }*/
+    public @ResponseBody Iterable<Todo> getTodoList(){
+        Iterable<TodoEntity> todoEntityIterable = this.todoRepository.findAll();
+        ArrayList<Todo> todoArrayList = new ArrayList<>();
+        for (TodoEntity todo : todoEntityIterable) {
+            todoArrayList.add(new Todo(todo.message, todo.author));
+        }
+        return todoArrayList;
     }
 }
